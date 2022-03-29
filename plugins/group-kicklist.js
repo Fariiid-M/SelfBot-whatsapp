90a3adfad8221d89f91b786f0aeb02529ebb1c6d
FaridMhrdkaa <85453179+Fariiid-M@users.noreply.github.com>
2022-03-29 18:55:01 +0700

    litle changes

--- plugins/group-kicklist.js
+++ plugins/group-kicklist.js
@@ -1,30 +1,29 @@
 let handler = async (m, { conn, args, participants }) => {
     if (!args[0]) throw `*Masukan kode nomornya yang mau di kick masal hehe..*`
-    let ownerGroup = m.chat.split("-")[0] + '@s.whatsapp.net' || groupMetadata.owner
-    
+    let nom = parseInt(args[0]?.substring(0, 4);
     // detect target
-    let array = []
-    for (let i of participants){
-        i.jid.startsWith(parseInt(args[0]?.substring(0, 4))) ? array.push(i.jid) : ''
-    }
-
-    //kick target
-    for (let user of array){
-        if (user?.endsWith('@s.whatsapp.net')){
-            let userr = participants.find(u => u.jid == user)
+    let member = participants.filter(({jid}) => jid.startsWith(nom)))
+    
+    await conn.reply(m.chat, `*${nom}*\n\n@${member.map(({jid, isAdmin, isSuperAdmin}) => jid + ` ( ${isSuperAdmin ? 'Owner Group' : isAdmin ? 'Admin Group'} )`)}`, m, { contextInfo: { mentionedJid: member.map(({jid}) => jid)}})
+    
+   //kick target
+    for (let user of member){
+        if (user.jid?.endsWith('@s.whatsapp.net')){
             delay(5000)
-            if (user == conn.user.jid) continue
-            if (user == m.sender) continue
-            if ((userr.isAdmin || userr.isSuperAdmin)) continue
-              conn.groupRemove(m.chat, [user])
+            if (user.jid == conn.user.jid) continue
+            if (user.jid == m.sender) continue
+            if ((user.isAdmin || user.isSuperAdmin)) continue
+              conn.groupRemove(m.chat, [user.jid])
         } 
     }
 }
 handler.tags = ['owner']
 handler.help = ['kicklist <kode-nomor>']
 handler.command = /^(kicklist|listkick)$/i
+
 handler.owner = true
 handler.botAdmin = true
+
 module.exports = handler
 
 
