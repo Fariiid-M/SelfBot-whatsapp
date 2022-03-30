a69bbd6e255951b02cb525f4ff988c132c4e5060
FaridMhrdkaa <85453179+Fariiid-M@users.noreply.github.com>
2022-03-30 23:57:12 +0700

    little change

--- plugins/tools-ssweb.js
+++ plugins/tools-ssweb.js
@@ -2,12 +2,12 @@ let handler = async (m, { conn, args, usedPrefix, command }) => {
     if (args.length == 0) throw conto(usedPrefix, command, `Masukan link yang ingin di screenshoot!!`, inUrl, 'https://google.com')
    let una = /https?:\/\//.test(args[0]) ? args[0] : 'https://' + args[0]
     try {
-      conn.sendFile(m.chat, 'https://nurutomo.herokuapp.com/api/ssweb?url='+una, 'ssweb.png', una, m)
+      conn.sendFile(m.chat, API('https://nurutomo.herokuapp.com', '/api/ssweb', { url: una, delay: 1000, type: 'png'}), 'ssweb.png', una, m)
     } catch{
       throw erorlink
     }
   }
   handler.tags = ['tools']
-  handler.help = ['ssweb'].map(v=>v+` ${inUrl}`)
+  handler.help = ['ssweb'].map(v => v + ` ${inUrl}`)
   handler.command = /^ss(web)?$/i
   module.exports = handler
