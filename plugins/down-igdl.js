5a3b04618efdccaf8b742f792426e3797c174c05
FaridMhrdkaa <85453179+Fariiid-M@users.noreply.github.com>
2022-05-06 02:54:40 +0700

    support ig stories??

--- plugins/down-igdl.js
+++ plugins/down-igdl.js
@@ -53,4 +53,4 @@ function isUrl(url){
 //     module.exports = handler
 //     function isUrl(url){
 //         return url.match(new RegExp(/https?:\/\/(www\.)?[-a-zA-Z0-9@:%._+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_+.~#?&/=]*)/, 'gi'))
-//     }
+//     } 
